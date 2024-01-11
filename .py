e1f8e6f254557d5828a11cbc47535fb40decd80b
abishekvp <abisihek.vp13@gmail.com>
2024-01-11 14:46:42 +0530

    update

--- .py
+++ .py
@@ -1,8 +1,7 @@
 import os
 
 os.system('git init')
-os.system('git clone https://github.com/abishekvp/asmi.git')
-os.chdir(os.path.expanduser('asmi'))
-os.system('pip install -r req.txt')
-os.chdir(os.path.expanduser('main'))
+os.system('git clone https://github.com/abishekvp/django.git')
+os.chdir(os.path.expanduser('django'))
+os.system('pip install -r requirements.txt')
 os.system('python -m manage runserver')
